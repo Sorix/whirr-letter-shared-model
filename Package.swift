3cc3b5d25a572984e3e93b7a5680a6ed0c74fa78
Vasily Ulianov <vasily@me.com>
2018-04-27 17:14:30 +0300

    Rename to SharedModel

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "whirr-letter-shared-model",
+    name: "SharedModel",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "shared-model",
-            targets: ["shared-model"]),
+            name: "SharedModel",
+            targets: ["SharedModel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "shared-model",
+            name: "SharedModel",
 			dependencies: [],
 			path: "Sources")
     ]
