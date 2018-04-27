0e4d887b87309632dae1d676a667d9ef59c9e798
Vasily Ulianov <vasily@me.com>
2018-04-27 17:12:15 +0300

    Updated model

--- Sources/Model.swift
+++ Sources/Model.swift
@@ -33,28 +33,64 @@ open class Letter: Codable {
 		self.extras = extras
 	}
 	
-}
-
-open class Address: Codable {
+	// MARK: -
 	
-	public var name: String?
-	public var addressLine1: String?
-	public var addressLine2: String?
-	public var townCity: String?
-	public var postCode: String?
+	open class Address: Codable {
+		
+		public var name: String?
+		public var addressLine1: String?
+		public var addressLine2: String?
+		public var townCity: String?
+		public var postCode: String?
+		
+		// MARK: Initializing an Item
+		
+		public init() { }
+	}
 	
-	// MARK: Initializing an Item
-	
-	public init() { }
-}
-
-open class Sender: Codable {
-	
-	public var address: String
-	public var includeSenderAddressOnEnvelope: Bool = false
-	
-	public init(address: String) {
-		self.address = address
+	open class Sender: Codable {
+		
+		public var address: String
+		public var includeSenderAddressOnEnvelope: Bool = false
+		
+		public init(address: String) {
+			self.address = address
+		}
+		
 	}
 	
 }
+
+public enum Precharge {
+	
+	open class Request: Codable {
+		public var zoneID: Int
+		public var paperTypeID: Int
+		public var numberOfPages: Int
+		public var zoneOfferID: Int
+		public var envelopeTypeID: Int
+		
+		public init(zoneID: Int, paperTypeID: Int, numberOfPages: Int, zoneOfferID: Int, envelopeTypeID: Int) {
+			self.zoneID = zoneID
+			self.paperTypeID = paperTypeID
+			self.numberOfPages = numberOfPages
+			self.zoneOfferID = zoneOfferID
+			self.envelopeTypeID = envelopeTypeID
+		}
+	}
+	
+	public struct Answer: Codable {
+		public let id: String
+		public let letterCost: Double
+		
+		public init(id: String, letterCost: Double) {
+			self.id = id
+			self.letterCost = letterCost
+		}
+	}
+	
+}
+
+
+
+
